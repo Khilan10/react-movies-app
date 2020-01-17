ec1b0e8c1695572492d39fa26c5980e2ae860b85
Khilan10 <mahajankhilan1@gmail.com>
2020-01-17 12:48:17 +0530

    - Added logo

--- movies-app/src/common/header/Header.js
+++ movies-app/src/common/header/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import './Header.css';
+import logo from '../../assets/logo.svg';
 
 class Header extends Component {
     render() {
         return (
             <div>
                 <div className="header-container">
+                    <img src={logo} className="app-logo" alt="Movies App Logo" />
                     <div className="btn-login">
                         <Button variant="contained" color="default">
                             Login
