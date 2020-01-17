e01cd57a450f38cf083e20d4ac7919ae93aa4870
Khilan10 <mahajankhilan1@gmail.com>
2020-01-17 12:31:28 +0530

    - Add Style to "Header" and aligned "Login" Button

--- movies-app/src/common/header/Header.js
+++ movies-app/src/common/header/Header.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import './Header.css';
 
 class Header extends Component {
     render() {
         return (
             <div>
-                <Button variant="contained" color="default">
-                    Login
+                <div className="header-container">
+                    <div className="btn-login">
+                        <Button variant="contained" color="default">
+                            Login
                 </Button>
+                    </div>
+                </div>
             </div>
         )
     }
