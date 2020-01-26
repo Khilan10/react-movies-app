b31593dd2deacdf84cef2c2f6ac39133d1d8cc92
Khilan10 <mahajankhilan1@gmail.com>
2020-01-26 15:48:31 +0530

    - Added ‘Release Date Start’ filter for movies on home page

--- movies-app/src/screens/home/Home.js
+++ movies-app/src/screens/home/Home.js
@@ -18,6 +18,7 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
 
 
 const styles = theme => ({
@@ -143,6 +144,9 @@ class Home extends Component {
                                             </MenuItem>
                                         ))}
                                     </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <TextField id="start-date" label="Release Date Start" type="date" defaultValue="" InputLabelProps={{ shrink: true }}></TextField>
                                 </FormControl>
                             </CardContent>
                         </Card>
