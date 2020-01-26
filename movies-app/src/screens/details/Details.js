c15117f7358566ec3899554418ba3cbf4e33bd85
Khilan10 <mahajankhilan1@gmail.com>
2020-01-26 19:20:39 +0530

    - Added 'Back To Home' button and YouTube trailer on details page

--- movies-app/src/screens/details/Details.js
+++ movies-app/src/screens/details/Details.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import moviesData from '../../common/moviesData';
 import Header from '../../common/header/Header';
 import './Details.css';
 import Typography from '@material-ui/core/Typography';
+import Home from '../home/Home';
+import Youtube from 'react-youtube';
 
 class Details extends Component {
     constructor() {
@@ -19,10 +22,23 @@ class Details extends Component {
         this.setState({ currentState });
         console.log(this.state);
     }
+    backToHomeHandler() {
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
     render() {
         let movie = this.state.movies;
+        const opts = {
+            length: '300',
+            width: '700',
+            playerVars: {
+                autoplay: 1
+            }
+        }
         return (
             <div className="details"><Header />
+                <div className="back" onClick={this.backToHomeHandler}>
+                    &#60; Back to Home
+            </div>
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
                         <img src={movie.poster_url} alt={movie.title}></img>
@@ -57,6 +73,10 @@ class Details extends Component {
                                 <span className="bold">Plot:</span><a href={movie.wiki_url}>(Wiki Link)</a>
                                 {movie.storyline}
                             </Typography>
+                        </div>
+                        <div className="trailerContainer">
+                            <Typography><span className="bold">Trailer:</span></Typography>
+                            <Youtube videoId={movie.trailer_url.split('?v=')[1]} opts={opts} onReady={this._onReady}></Youtube>
                         </div>
                     </div>
                     <div className="rightDetails">
