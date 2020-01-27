c7e01bd76b40abdb8477da7c7a7e51e9832094c5
Khilan10 <mahajankhilan1@gmail.com>
2020-01-27 23:19:27 +0530

    - Added 'Back to Movie Details' button on book show page

--- movies-app/src/screens/bookShow/BookShow.js
+++ movies-app/src/screens/bookShow/BookShow.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
+import Home from '../home/Home';
+import Typography from '@material-ui/core/Typography'
+import './BookShow.css';
 
 class BookShow extends Component {
+    backToDetailsHandler() {
+        ReactDOM.render(<Home />, document.getElementById('root'));
+    }
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <div className="back-btn-container">
+                    <Typography className="back" onClick={this.backToDetailsHandler}>
+                        &#60; Back to Movie Details
+                    </Typography>
+                </div>
+            </div>
         );
     }
 }
